243cbb43f4e1d5cad163c4ca160e130eafa010e8
Manglesh <manglesh0605@gmail.com>
2022-03-03 19:05:50 +0530

    made userSchema and added middleware in server

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 
+//the db connection using mongoose--
+const connectDB = require('./config/db');
+
+//connecting to db-
+connectDB();
+
+//initializing middleware--
+app.use(express.json({extended:false}));
+
 app.get('/', (req, res) => {
   res.send('API RUNNING');
 });
+
+//DEFINE ROUTES__
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/post'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 //port to listen the server --
 const PORT = process.env.PORT || 5000;
 
